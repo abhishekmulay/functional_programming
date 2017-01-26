6b65b22ea67ab4f86d2d5d2f00555102752cf189
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-01-26 18:30:35 -0500

    Data design, examples and tests

--- set02/q1.rkt
+++ set02/q1.rkt
@@ -16,21 +16,49 @@
 
 
 ;; DATA DEFINITIONS:
+
 (define-struct editor (pre post))
 
 ;; An Editor is a
-;; (make-editor String String)
+;;   (make-editor String String)
 ;; INTERPRETATION:
 ;;   An editor is made of two parts, we consider the editor holds two strings which are separated by a virtual caret.
 ;;   Pre: it is the part of string which is to the left of virtual caret
 ;;   Post: it is the part of string which is to the right of virtual caret
-
-;; edit : Editor + KeyEvent -> Editor 
+;;
+;; Observer template:
+;; ------------------
+;; editor-fn : Editor -> ??
+;;  (define (editor-fn ed)
+;;    (...
+;;     (editor-pre ed)
+;;       (editor-post ed)))
+;;
+;;
+;;
+;; edit : Editor + KeyEvent -> Editor
+;;
 ;; GIVEN:
 ;;    An Editor ed with two string members "pre" and "post"
 ;;    A KeyEvent ke 
 ;; RETURNS: An Editor with changes made to the position of caret caused by the provided KeyEvent ke
+;;
+;; EXAMPLE:
+;;   (edit (make-editor "Abhishek" "Mulay") "\b") = (make-editor "Abhishe" "Mulay")
+;;   (edit (make-editor "Abhishek" "Mulay") "left") = (make-editor "Abhishe" "kMulay")
+;;   (edit (make-editor "Abhishek" "Mulay") "right") = (make-editor "AbhishekM" "ulay")
+
 ;; DESIGN STRATEGY: Divide into cases based on cond
 
 
+
+;; TESTS:
+(begin-for-test
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "\b") (make-editor "Abhishe" "Mulay") " '\b' should delete last char in pre field of editor")
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "\t") (make-editor "Abhishek" "Mulay") "'\t' should be ignored")
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "\r") (make-editor "Abhishek" "Mulay") "'\r' should be ignored")
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "left") (make-editor "Abhishe" "kMulay") "For 'left' caret should move one character towards left")
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "right") (make-editor "AbhishekM" "ulay") "For 'right' caret should move one character towards right")
+  (check-equal? (edit (make-editor "Abhishek" "Mulay") "a") (make-editor "Abhisheka" "Mulay") " should append other character to end of pre field of editor")
+)
 
