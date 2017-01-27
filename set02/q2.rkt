5ceb0160cdd4a87aafe10d91b4d0c73d3dd612bd
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-01-27 11:20:18 -0500

    Added Data Design and Interpretation for State

--- set02/q2.rkt
+++ set02/q2.rkt
@@ -6,10 +6,13 @@
 (check-location "02" "q2.rkt")
 (check-location "02" "extras.rkt")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; DATA DEFINITION:
-     (define-struct legal-input (char))
-     
+
+(define-struct legal-input (char))
+
+;; A LegalInput is (make-legal-input 1String)
 ;; A LegalInput is one of:
 ;;     'd'
 ;;     'e'
@@ -17,24 +20,35 @@
 ;;     's'
 ;; INTERPRETATION:
 ;;    Self evident
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; DATA DEFINITION:
+
+ (define-struct state (id input))
+
+;; A State is (make-state (NonNegNumber LegalInput))
+;; A State is one of:
+;;  -- (make-state id input)
+;; INTERPRETATION:
+;;   id is a NonNegNumber identifier for the state, ex. 0, 1, 2 etc.
+;;   input is a LegalInput accepted by this state
+;;   According to the Finite State Automata design in file q2.jpg
+;;   Start state is: 0
+;;   Accepting/final states are: 2, 4, 5
+;; 
+;;   id: 0 accepts: d, p, s  rejects: e
+;;   id: 1 accepts: d, p     rejects: e, s
+;;   id: 2 accepts: d, e     rejects: s, p
+;;   id: 3 accepts: d        rejects: e, p, s
+;;   id: 4 accepts: -        rejects: d, e, s, p
+;;   id: 5 accepts: d, e     rejects: p, s
 ;;
-     
-;; initial-state : Number -> State
-;; GIVEN: a number
-;; RETURNS: a representation of the initial state
-;; of your machine.  The given number is ignored.
-;;          
-;; next-state : State LegalInput -> State
-;; GIVEN: a state of the machine and a machine input
-;; RETURNS: the state the machine should enter if it
-;; is in the given state and sees the given input.
-;;          
-;; accepting-state? : State -> Boolean
-;; GIVEN: a state of the machine
-;; RETURNS: true iff the given state is a final (accepting) state
-;;          
-;; rejecting-state? : State -> Boolean
-;; GIVEN: a state of the machine
-;; RETURNS: true iff there is no path (empty or non-empty) from the given state to an accepting state
-        
-     
+;;   State "accepts" a LegalInput means there is an outgoing path for that LegalInput from that state
+;;   in the FSM diagram q2.png
+;;
+;;   State "rejects" a LegalInput means there is NO outgoing path for that LegalInput from that state
+;;   in the FSM diagram q2.png
+ 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ 
