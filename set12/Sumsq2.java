d3307963653f17ce7dc9206ec04b759a2553d7f1
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-04-16 14:38:45 -0400

    Sumsq2 done

--- set12/Sumsq2.java
+++ set12/Sumsq2.java
@@ -6,52 +6,46 @@
 //
 // Usage:
 //
-//     java Sumsq N ITERS
+//     java Sumsq2 N ITERS
 
 class Sumsq2 {
 
-    public static void main (String[] args) {
-	long n = Long.parseLong (args[0]);
-	long iters = Long.parseLong (args[1]);
-	System.out.println (mainLoop (n, iters));
+    public static void main(String[] args) {
+        long n = Long.parseLong(args[0]);
+        long iters = Long.parseLong(args[1]);
+        System.out.println(mainLoop(n, iters));
     }
 
     // Modify this method to use loop syntax instead of tail recursion.
+    static long mainLoop(long n, long iters) {
+        for (long iteration = iters; iteration >= 0; iteration--) {
+            if (iteration == 0) {
+                return 0 - 1;
+            }
 
-	static long mainLoop(long n, long iters) {
-		if (iters == 0)
-			return 0 - 1;
-		else if (iters == 1)
-			return sumSquares(n);
-		else {
-			sumSquares(n);
-			return mainLoop(n, iters - 1);
-		}
-	}
+            if (iteration == 1) {
+                return sumSquares(n);
+
+            } else {
+                sumSquares(n);
+            }
+        }
+        return -1;
+    }
 
     // Returns alternating sum of the first n squares.
-
-    static long sumSquares (long n) {
-	return sumSquaresLoop (n, 0);
-    }
-
-    // Modify the following methods to use loop syntax
-    // instead of tail recursion.
-
-    // Returns alternating sum of the first n+1 squares, plus sum.
-
-    static long sumSquaresLoop (long n, long sum) {
-	if (n < 2)
-	    return sum + n * n;
-	else return sumSquaresLoop2 (n - 1, sum + n * n);
-    }
-
-    // Returns alternating sum of the first n+1 squares,
-    // minus (n+1)^2, plus sum.
-
-    static long sumSquaresLoop2 (long n, long sum) {
-	if (n < 2)
-	    return sum - n * n;
-	else return sumSquaresLoop (n - 1, sum - n * n);
+    static long sumSquares(long n) {
+        long result = 0;
+        for (long iteration = 0; iteration < n; iteration++) {
+            // even
+            if (iteration % 2 == 0) {
+                result += (n - iteration) * (n - iteration);
+            }
+            // odd
+            else {
+                result -= (n - iteration) * (n - iteration);
+            }
+        }
+        return result;
     }
 }
