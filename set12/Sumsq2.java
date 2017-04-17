e01a99473c0798d88825379c558ea7d193fc6940
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-04-17 13:30:11 -0400

    Added essay

--- set12/Sumsq2.java
+++ set12/Sumsq2.java
@@ -1,3 +1,5 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 // Returns the alternating sum of the first n squares, computed ITERS times.
 //
 // Result is
@@ -6,7 +8,7 @@
 //
 // Usage:
 //
-//     java Sumsq2 N ITERS
+//     java Sumsq N ITERS
 
 class Sumsq2 {
 
@@ -16,14 +18,22 @@ class Sumsq2 {
         System.out.println(mainLoop(n, iters));
     }
 
-    // Modify this method to use loop syntax instead of tail recursion.
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //          This method now uses loop syntax instead of tail recursion.                  //
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    // given: number n that represents number of terms in equation and number that represents number of iterations
+    // returns: the alternating sum of the first n squares, computed ITERS times.
+    // where: Following equation gives value of mainLoop(n) for any n > 0
+    //
+    //     n*n - (n-1)*(n-1) + (n-2)*(n-2) - (n-3)*(n-3) + ...
+    // Pattern: Functional visitor pattern
     static long mainLoop(long n, long iters) {
         for (long iteration = iters; iteration >= 0; iteration--) {
             if (iteration == 0) {
                 return 0 - 1;
-            }
 
-            if (iteration == 1) {
+            } else if (iteration == 1) {
                 return sumSquares(n);
 
             } else {
@@ -35,17 +45,27 @@ class Sumsq2 {
 
     // Returns alternating sum of the first n squares.
     static long sumSquares(long n) {
-        long result = 0;
-        for (long iteration = 0; iteration < n; iteration++) {
-            // even
-            if (iteration % 2 == 0) {
-                result += (n - iteration) * (n - iteration);
-            }
-            // odd
-            else {
-                result -= (n - iteration) * (n - iteration);
-            }
+        return sumSquaresLoop(n, 0);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    //  Following methods have been modified to use loop syntax instead of tail recursion.  //
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    // Returns alternating sum of the first n+1 squares, plus sum.
+    static long sumSquaresLoop(long n, long sum) {
+        for (long index = n; index > 0; index -= 2) {
+            sum = sum + index * index;
         }
-        return result;
+        return sumSquaresLoop2(n - 1, sum);
+    }
+
+    // Returns alternating sum of the first n+1 squares, minus (n+1)^2, plus sum.
+    static long sumSquaresLoop2(long n, long sum) {
+        for (long index = n; index > 0; index -= 2) {
+            sum = sum - index * index;
+        }
+        return sum;
     }
 }
+////////////////////////////////////////////////////////////////////////////////////////////
